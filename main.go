e05ce058c7661890dda049fe7a788e46b103b696
Asim <asim@chuhnk.me>
2016-01-02 20:22:00 +0000

    Use service definition

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
 	log "github.com/golang/glog"
-	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/server"
+	"github.com/micro/cli"
+	micro "github.com/micro/go-micro"
 
 	"github.com/micro/geocode-srv/google"
 	"github.com/micro/geocode-srv/handler"
@@ -12,39 +11,37 @@ import (
 )
 
 func main() {
-	cmd.Flags = append(cmd.Flags,
-		cli.StringFlag{
-			Name:   "google_api_key",
-			EnvVar: "GOOGLE_API_KEY",
-			Usage:  "Google maps API key",
-		},
-		cli.StringFlag{
-			Name:   "google_client_id",
-			EnvVar: "GOOGLE_CLIENT_ID",
-			Usage:  "Google client id",
-		},
-		cli.StringFlag{
-			Name:   "google_signature",
-			EnvVar: "GOOGLE_SIGNATURE",
-			Usage:  "Google signature",
-		},
+	service := micro.NewService(
+		micro.Name("go.micro.srv.geocode"),
+		micro.Flags(
+			cli.StringFlag{
+				Name:   "google_api_key",
+				EnvVar: "GOOGLE_API_KEY",
+				Usage:  "Google maps API key",
+			},
+			cli.StringFlag{
+				Name:   "google_client_id",
+				EnvVar: "GOOGLE_CLIENT_ID",
+				Usage:  "Google client id",
+			},
+			cli.StringFlag{
+				Name:   "google_signature",
+				EnvVar: "GOOGLE_SIGNATURE",
+				Usage:  "Google signature",
+			},
+		),
+		micro.Action(func(c *cli.Context) {
+			google.Key = c.String("google_api_key")
+			google.ClientID = c.String("google_client_id")
+			google.Signature = c.String("google_signature")
+		}),
 	)
 
-	cmd.Actions = append(cmd.Actions, func(c *cli.Context) {
-		google.Key = c.String("google_api_key")
-		google.ClientID = c.String("google_client_id")
-		google.Signature = c.String("google_signature")
-	})
+	service.Init()
 
-	cmd.Init()
+	proto.RegisterGoogleHandler(service.Server(), new(handler.Google))
 
-	server.Init(
-		server.Name("go.micro.srv.geocode"),
-	)
-
-	proto.RegisterGoogleHandler(server.DefaultServer, &handler.Google{})
-
-	if err := server.Run(); err != nil {
+	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
